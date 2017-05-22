1c6bd914a8bb168402a9bcd9682371661f712e9a
Lauren Hutchinson <Lauren Hutchinson>
2017-05-22 18:31:02 -0500

    finished timer, div, and counter

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,27 +1,41 @@
-
+$(document).ready(function(){
 
 //counter for the timer
 var counter= 10;
 
 //end display results
 var correctAnswers=0
-$("#correctAnswers").html(correctAnswers);
+$("#correctAnswers").append(correctAnswers);
 var incorrectAnswers=0
-$("#incorrectAnswers").html(incorrectAnswers);
+$("#incorrectAnswers").append(incorrectAnswers);
 var unanswered=0
-$("#unanswered").html(unanswered);
+$("#unanswered").append(unanswered);
 
 //correct answers
+
+// var allCorrectAnswers = ["#Q1inlineRadio3","#Q2inlineRadio2", "#Q3inlineRadio4", "#Q4inlineRadio1"];
+
 var answerQuestion1= $("#Q1inlineRadio3").parent().text().trim();
 var answerQuestion2= $("#Q2inlineRadio2").parent().text().trim();
 var answerQuestion3= $("#Q3inlineRadio4").parent().text().trim();
 var answerQuestion4= $("#Q4inlineRadio1").parent().text().trim();
 
 //showing the all done div when clicking the done button
+//allDone").hide();
 
-$("#done").click(function() {
-  $("#allDone").toggle();
-});
+    $("#done").click(function(e) {
+      e.preventDefault();
+      $("#allDone").show();
+
+        if ($("input[type='radio']:checked").val()){
+            console.log('checked');
+            console.log("unanswered: " + unanswered)}
+            else {
+            unanswered++;
+            $("#unanswered").html("Unanswered: " + unanswered);
+            console.log('nothing checked');}
+    });
+ 
 
 //on clicking the start button, the timer begins
 $("#start").click(function() {
@@ -52,39 +66,115 @@ $("input[name='inlineRadioOptions']").click(function(answer) {
         if (chosenAnswer== answerQuestion1){
           console.log("correct")
           correctAnswers++;
-          $("#correctAnswers").append(correctAnswers);
+          $("#correctAnswers").html("Correct Answers: " + correctAnswers);
           console.log("Number of correct answers: " + correctAnswers);}
 
           else{
             console.log(chosenAnswer.text+"|"+answerQuestion1);
             console.log("incorrect")
             incorrectAnswers++;
-            $("#incorrectAnswers").append(incorrectAnswers);
+            $("#incorrectAnswers").html("Incorrect Answers: " + incorrectAnswers);
             console.log("Number of incorrect answers: " + incorrectAnswers);}
 
-          if ($("input[name='inlineRadioOptions']:checked").val()){
+          if ($("input[type='radio']:checked").val()){
             console.log('checked');
             console.log("unanswered: " + unanswered)}
             else {
             unanswered++;
-            $("#unanswered").append(unanswered);
+            $("#unanswered").html("Unanswered: " + unanswered);
             console.log('nothing checked');}
           });
 
-      
+$("input[name='inlineRadioOptions2']").click(function(answer) {
 
+//declaring variable chosen
+    var chosenAnswer = $(this).parent().text().trim();
+    //var chosenAnswer = $("input[name='inlineRadioOptions']").parent().text();//(answer.target.id);
+    console.log("chosen: " + answer.target.id);
 //question 2
-      //   if (chosenAnswer== answerQuestion2){
-      //     console.log("correct")
-      //     correctAnswers++;
-      //     console.log("Number of correct answers: " + correctAnswers)
-      //   }
-      //   else{
-      //     console.log(chosenAnswer.text+"|"+answerQuestion1);
-      //     console.log("incorrect")
-      //     incorrectAnswers++;
-      //     console.log("Number of incorrect answers: " + incorrectAnswers)
-      //   }
-      // });
+        if (chosenAnswer== answerQuestion2){
+          console.log("correct")
+          correctAnswers++;
+          $("#correctAnswers").html("Correct Answers: " + correctAnswers);
+          console.log("Number of correct answers: " + correctAnswers);}
+
+          else{
+            console.log(chosenAnswer.text+"|"+answerQuestion2);
+            console.log("incorrect")
+            incorrectAnswers++;
+            $("#incorrectAnswers").html("Incorrect Answers: " + incorrectAnswers);
+            console.log("Number of incorrect answers: " + incorrectAnswers);}
+
+          if ($("input[type='radio']:checked").val()){
+            console.log('checked');
+            console.log("unanswered: " + unanswered)}
+            else {
+            unanswered++;
+            $("#unanswered").html("Unaswered: " + unanswered);
+            console.log('nothing checked');}
+          });
 
 
+//question 3
+$("input[name='inlineRadioOptions3']").click(function(answer) {
+
+//declaring variable chosen
+    var chosenAnswer = $(this).parent().text().trim();
+    //var chosenAnswer = $("input[name='inlineRadioOptions']").parent().text();//(answer.target.id);
+    console.log("chosen: " + answer.target.id);
+
+        if (chosenAnswer== answerQuestion3){
+          console.log("correct")
+          correctAnswers++;
+          $("#correctAnswers").html("Correct Answers: " + correctAnswers);
+          console.log("Number of correct answers: " + correctAnswers);}
+
+          else{
+            console.log(chosenAnswer.text+"|"+answerQuestion3);
+            console.log("incorrect")
+            incorrectAnswers++;
+            $("#incorrectAnswers").html("Incorrect Answers: " + incorrectAnswers);
+            console.log("Number of incorrect answers: " + incorrectAnswers);}
+
+          if ($("input[type='radio']:checked").val()){
+            console.log('checked');
+            console.log("unanswered: " + unanswered)}
+            else {
+            unanswered++;
+            $("#unanswered").html("Unanswered: " + unanswered);
+            console.log('nothing checked');}
+          });
+
+
+//question 4
+
+$("input[name='inlineRadioOptions4']").click(function(answer) {
+
+//declaring variable chosen
+    var chosenAnswer = $(this).parent().text().trim();
+    //var chosenAnswer = $("input[name='inlineRadioOptions']").parent().text();//(answer.target.id);
+    console.log("chosen: " + answer.target.id);
+
+
+        if (chosenAnswer== answerQuestion4){
+          console.log("correct")
+          correctAnswers++;
+          $("#correctAnswers").html("Correct Answers: " + correctAnswers);
+          console.log("Number of correct answers: " + correctAnswers);}
+
+          else{
+            console.log(chosenAnswer.text+"|"+answerQuestion4);
+            console.log("incorrect")
+            incorrectAnswers++;
+            $("#incorrectAnswers").html("Incorrect Answers: " + incorrectAnswers);
+            console.log("Number of incorrect answers: " + incorrectAnswers);}
+
+          if ($("input[type='radio']:checked").val()){
+            console.log('checked');
+            console.log("Unanswered: " + unanswered)}
+            else {
+            unanswered++;
+            $("#unanswered").html("Unanswered: " + unanswered);
+            console.log('nothing checked');}
+          });
+ });
